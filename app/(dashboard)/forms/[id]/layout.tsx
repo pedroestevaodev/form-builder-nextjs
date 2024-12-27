9afae7b516b7896c2559aa3259623a81c17ea58c
Pedro-Estevao <contato@pedroestevao.com>
2024-12-27 19:14:03 -0300

    Update layout.tsx

--- app/(dashboard)/forms/[id]/layout.tsx
+++ app/(dashboard)/forms/[id]/layout.tsx
@@ -2,7 +2,7 @@ import { ChildrenProps } from "@/types/nextjs";
 
 const BuilderLayout = ({ children }: Readonly<ChildrenProps>) => {
     return (
-        <div className="flex flex-col w-full flex-grow mx-auto">
+        <div className="flex flex-col w-full flex-grow container mx-auto">
             {children}
         </div>
     );
